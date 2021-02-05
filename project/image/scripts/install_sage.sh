97c21181c5747ab58c95b8195e384924c39e506c
Felix Höddinghaus <info@xefli.de>
2021-02-05 13:40:23 +0100

    Run ./configure after make configure

--- project/image/scripts/install_sage.sh
+++ project/image/scripts/install_sage.sh
@@ -28,6 +28,7 @@ cd sage
 
 # This may fail: https://trac.sagemath.org/ticket/23519
 make configure
+./configure
 make || true
 # Because of "stupid" static GMP's get left around that break the build.
 # So we try again with the static GMP's removed.
