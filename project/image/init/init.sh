5ca81f1f6b34887a889fd1a332c3902b61c72647
Felix Höddinghaus <info@xefli.de>
2021-04-01 17:52:21 +0200

    Update init.sh

--- project/image/init/init.sh
+++ project/image/init/init.sh
@@ -89,8 +89,12 @@ rm -rf "$SMC" && mkdir "$SMC"
 # Create .julia folder
 mkdir "$HOME/.julia" || true
 
-# Install Julia and all dependencies
-julia /cocalc/init/julia_init.jl
+# Cleanup the registry for fresh installation (at restart)
+#rm -rf "$HOME/.julia/registries" || true
+
+# Install IJulia and all dependencies
+julia /cocalc/init/julia_init.jl # < /dev/null > /dev/stdout 2> /dev/stderr &
+#disown
 
 bash /cocalc/kucalc-start-sshd.sh < /dev/null > /dev/stdout 2> /dev/stderr &
 disown
